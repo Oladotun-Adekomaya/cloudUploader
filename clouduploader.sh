8ed428198c121658bf9306ac98225aee237f9fbd
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2024-06-15 12:05:40 +0100

    edited the checksource function in clouduploader.sh

--- clouduploader.sh
+++ clouduploader.sh
@@ -4,17 +4,27 @@ source=$1
 
 # Function to check if the file/directory to be uploaded is not available
 checkSource (){
-   if ! [ -e $1 ]; then
+   if ! [ -e $source ]; then
       echo "File/Directory does not exist."
    fi
 }
 
+checkSource
 # Function to check if the file/directory to be uploaded to the cloud already exists
 checkDestination (){
     aws s3 ls s3://oladotun-first-bucket \
     --recursive | grep -w "file1.txt"
 }
 
-checkDestination $source
 
+getFileNameFromSourcePath (){
+   IFS="/"
+   read -ra sourcePathArr <<< "$source"
+   arrLength=${#sourcePathArr[@]}
+   n=$(($arrLength-1))
+   fileName=${sourcePathArr[$n]}
+   echo {$fileName}
 
+}
+
+getFileNameFromSourcePath
