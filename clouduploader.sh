2f8039d5abc9268fddfb373c782065334aaaf6db
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2024-06-15 10:15:18 +0100

    added a few comments to describe the created functions

--- clouduploader.sh
+++ clouduploader.sh
@@ -2,12 +2,14 @@
 
 source=$1
 
+# Function to check if the file/directory to be uploaded is not available
 checkSource (){
    if ! [ -e $1 ]; then
       echo "File/Directory does not exist."
    fi
 }
 
+# Function to check if the file/directory to be uploaded to the cloud already exists
 checkDestination (){
     aws s3 ls s3://oladotun-first-bucket \
     --recursive | grep -w "file1.txt"
